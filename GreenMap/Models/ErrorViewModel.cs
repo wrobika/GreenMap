16f0e600906a96cec68a81a6553c72252aa491bc
Weronika Naziemiec <wnaziemiec@gmail.com>
2020-03-13 10:56:20 +0100

    1.Initial commit 2.Frontend

---
+++ GreenMap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreenMap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
