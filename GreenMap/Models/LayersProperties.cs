5e2cfcb3be5224eb155cb898b4940c83b7655b78
Weronika <wnaziemiec@gmail.com>
2020-05-06 16:15:12 +0200

    hydro in 9 layers

--- GreenMap/Models/LayersProperties.cs
+++ GreenMap/Models/LayersProperties.cs
@@ -10,11 +10,19 @@ namespace GreenMap.Models
         public static Dictionary<string, Layer> Layers = new Dictionary<string, Layer>
         {
             { "mapa", new Layer{ Color = "Gray", Opacity = 1, Text="White"} },
-            { "miasto", new Layer{ Color = "OrangeRed", Opacity = 0.7, Stroke="OrangeRed", Text="White"} },
+            { "miasto", new Layer{ Color = "OrangeRed", Opacity = 0, Stroke="OrangeRed", Text="White"} },
             { "dzielnice", new Layer{ Color = "Gold", Opacity = 0.5, Stroke="Gold", Text="Black"} },
             { "zieleń", new Layer{ Color = "LimeGreen", Opacity = 0.7, Stroke="Green", Text="Black"} },
             { "cieki/zbiorniki", new Layer{ Color = "Aquamarine", Opacity = 1, Text="Black"} },
-            { "zwierciadło", new Layer{ Color = "DarkBlue", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "zwierciadłoI", new Layer{ Color = "PowderBlue", Opacity = 1, Text="Black", Cluster=true, Radius=11} },
+            { "zwierciadłoII", new Layer{ Color = "Cyan", Opacity = 1, Text="Black", Cluster=true, Radius=11} },
+            { "zwierciadłoIII", new Layer{ Color = "DarkTurquoise", Opacity = 1, Text="Black", Cluster=true, Radius=11} },
+            { "zwierciadłoIV", new Layer{ Color = "DodgerBlue", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "zwierciadłoV", new Layer{ Color = "DarkCyan", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "zwierciadłoVI", new Layer{ Color = "Blue", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "zwierciadłoVII", new Layer{ Color = "MediumSlateBlue", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "zwierciadłoVIII", new Layer{ Color = "DarkBlue", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "zwierciadłoIX", new Layer{ Color = "Black", Opacity = 1, Text="White", Cluster=true, Radius=11} },
             { "obiekty", new Layer{ Color = "DarkGoldenRod", Opacity = 1, Text="White", Cluster=true, Radius=10} },
             { "odwierty", new Layer{ Color = "Red", Opacity = 1, Text="White", Cluster=true, Radius=9} },
         };
