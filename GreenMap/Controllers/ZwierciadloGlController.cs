6d477694dcd624950b86c4953dd2f0bbe408d5b4
Weronika <wnaziemiec@gmail.com>
2020-05-07 15:05:39 +0200

    mirror in 1 layer with 9 classes

--- GreenMap/Controllers/ZwierciadloGlController.cs
+++ GreenMap/Controllers/ZwierciadloGlController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using GreenMap;
+using GreenMap.Models;
 using NetTopologySuite.Geometries;
 
 namespace GreenMap.Controllers
@@ -31,6 +31,35 @@ namespace GreenMap.Controllers
                  .Select(item => new Point(item.EurefY.Value, item.EurefX.Value).ToString())
                  .ToListAsync();
             return wkt;
+        }
+
+        public async Task<Dictionary<string, string>> GetZwierciadloGlebokosc()
+        //public async Task<List<ZwierciadloGl>> GetZwierciadloGlebokosc()
+        {
+            var points = await _context.ZwierciadloGl
+                 .Where(item => item.EurefX != null)
+                 .Where(item => item.EurefY != null)
+                 .Where(item => item.GlUstabilizowana != null)
+                 .Select(item => new { item.EurefX, item.EurefY, item.GlUstabilizowana})
+                 .ToListAsync();
+            //.ToDictionaryAsync(
+            //   item => new Point(item.EurefY.Value, item.EurefX.Value).ToString(),
+            //   item => item.GlUstabilizowana.Value);
+            var wktDepthDictionary = new Dictionary<string, string>();
+            foreach(var item in points)
+            {
+                string wkt = new Point(item.EurefY.Value, item.EurefX.Value).ToString();
+                //if (wktDepthDictionary.ContainsKey(wkt))
+                //{
+                //    if(wktDepthDictionary[wkt] > item.GlUstabilizowana.Value)
+                //        wktDepthDictionary[wkt] = item.GlUstabilizowana.Value;
+                //}
+                //else
+                //{
+                    wktDepthDictionary[wkt] = DepthColor.GetColor(item.GlUstabilizowana.Value);
+                //}
+            }
+            return wktDepthDictionary;
         }
 
         public async Task<List<string>> GetZwierciadloGl(decimal upperBound, decimal lowerBound)
---
+++ GreenMap/Models/DepthColor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenMap.Models
+{
+    public class DepthColor
+    {
+        public static string GetColor(decimal depth)
+        {
+            if (depth > -0.5M)
+                return "rgb(0,255,255)";
+            if (depth > -1M)
+                return "rgb(0,192,255)";
+            if (depth > -2M)
+                return "rgb(0,128,255)";
+            if (depth > -5M)
+                return "rgb(0,64,255)";
+            if (depth > -10M)
+                return "rgb(0,0,255)";
+            if (depth > -15M)
+                return "rgb(0,0,192)";
+            if (depth > -20M)
+                return "rgb(0,0,128)";
+            if (depth > -30M)
+                return "rgb(0,0,64)";
+            else
+                return "rgb(0,0,0)";
+        }
+        public static Dictionary<decimal, string> Ranges = new Dictionary<decimal, string>
+        {
+            { -0.5M, "rgb(0,255,255)" },
+            { -1M, "rgb(0,192,255)" },
+            { -2M, "rgb(0,128,255)" },
+            { -5M, "rgb(0,64,255)" },
+            { -10M, "rgb(0,0,255)" },
+            { -15M, "rgb(0,0,192)" },
+            { -20M, "rgb(0,0,128)" },
+            { -30M, "rgb(0,0,64)" },
+            { -1000M , "rgb(0,0,0)"},
+        };
+    }
+}
