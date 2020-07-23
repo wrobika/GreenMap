95cb807c2e845940ef7faf755cc9faf052a96d36
Weronika <wnaziemiec@gmail.com>
2020-07-23 11:32:50 +0200

    open monitoring pdf in a new tab

---
+++ GreenMap/Controllers/MonitoringController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GreenMap.Models;
+using Microsoft.AspNetCore.Authorization;
+using NetTopologySuite.Geometries;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GreenMap.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class MonitoringController : ControllerBase
+    {
+        private readonly epionierContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public MonitoringController(epionierContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        // GET: api/Monitoring
+        [Route("api/Monitoring")]
+        [HttpGet]
+        public async Task<Dictionary<string,string>> GetMonitoring()
+        {
+            var wktWithPdfName = await _context.Monitoring
+                .Where(item => item.X != null)
+                .Where(item => item.Y != null)
+                .ToDictionaryAsync(item => new Point(item.X.Value, item.Y.Value).ToString(),
+                    item => item.Nazwa);
+            return wktWithPdfName;
+
+            //var wktWithPdfName = await _context.ZanieczyszczenieGleb
+            //    .Where(item => item.X != null)
+            //    .Where(item => item.Y != null)
+            //    .ToDictionaryAsync(item => new Point(item.Y.Value, item.X.Value).ToString(),
+            //        item => item.Symbol);
+            //return wktWithPdfName;
+        }
+
+        [HttpGet]
+        [Route("api/Monitoring/OpenPdf/{name}")]
+        public IActionResult OpenPdf(string name)
+        {
+            string path = _env.WebRootPath + "/monitoringPdf/" + name + ".pdf";
+            return new PhysicalFileResult(path, "application/pdf");
+        }
+    }
+}
--- GreenMap/Models/LayersProperties.cs
+++ GreenMap/Models/LayersProperties.cs
@@ -18,9 +18,10 @@ namespace GreenMap.Models
                 UrlWMS = urlWMS, LayersWMS = "gleby:gleby_Biezanow_Prokocim,gleby:gleby_Bieñczyce,gleby:gleby_Czyżyny,gleby:gleby_NHuta,gleby:gleby_Podgorze"} },
             { "greenery", new Layer{ FullName = "tereny zielone", Visible = true, Color = "LimeGreen", Opacity = 0.7, Text="Black"} },
             { "hydro", new Layer{ FullName = "cieki/zbiorniki wodne", Visible = true, Color = "Aquamarine", Opacity = 1, Text="Black"} },
-            { "filter", new Layer{ FullName = "filtracja", Visible = false, Color = "Silver", Opacity = 1, Text="Black", Cluster=true, Radius=11} },
+            { "filter", new Layer{ FullName = "filtracja", Visible = false, Color = "Silver", Opacity = 1, Text="Black", Cluster=true, Radius=12} },
             { "hydroizohypse", new Layer{ FullName = "zwierciadło wód podziemnych", Visible = false, Color = "DarkBlue", Opacity = 1, Text="White", Cluster=false} },
-            { "rbdh", new Layer{ FullName = "baza otworów badawczych", Visible = true, Color = "DarkGoldenRod", Opacity = 1, Text="White", Cluster=true, Radius=10} },
+            { "rbdh", new Layer{ FullName = "baza otworów badawczych", Visible = true, Color = "DarkGoldenRod", Opacity = 1, Text="White", Cluster=true, Radius=11} },
+            { "monitoring", new Layer{ FullName = "monitoring", Visible = false, Color = "Violet", Opacity = 1, Text="Black", Cluster=true, Radius = 10} },
             { "drilling", new Layer{ FullName = "typ otworu", Visible = true, Color = "Red", Opacity = 1, Text="White", Cluster=true, Radius=9} },
         };
     }
